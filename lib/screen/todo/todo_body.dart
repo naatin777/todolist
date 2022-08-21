a2ccb9e6e4f20e7ac9f266a592c5777df7f9e1a9
tknk0369 <76672482+tknk0369@users.noreply.github.com>
2022-08-21 15:08:31 +0900

    Add list item

--- lib/screen/todo/todo_body.dart
+++ lib/screen/todo/todo_body.dart
@@ -6,22 +6,34 @@ class TodoBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final listItem = List.generate(50, (index) => index);
-    return ListView.builder(
+    return ListView.separated(
       itemCount: listItem.length,
       itemBuilder: (context, index) {
-        return InkWell(
+        return Dismissible(
           key: Key(listItem[index].toString()),
-          onTap: () {},
-          child: Row(
-            children: [
-              Checkbox(
-                value: false,
-                onChanged: (value) {},
+          child: ListTile(
+            leading: Checkbox(
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10.0),
+                ),
               ),
-              Text("${listItem[index]}")
-            ],
+              value: false,
+              onChanged: (value) {},
+            ),
+            title: Text("${listItem[index]}"),
+            subtitle: Text("subtitle"),
+            onTap: () {},
+            horizontalTitleGap: 0,
+            contentPadding: EdgeInsets.symmetric(horizontal: 4.0),
           ),
+          background: Container(color: Colors.blue),
+          secondaryBackground: Container(color: Colors.red),
+          onDismissed: (direction) {},
         );
+      },
+      separatorBuilder: (context, index) {
+        return Container();
       },
     );
   }
