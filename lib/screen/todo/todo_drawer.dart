44559f540000a498748df229d3e182631b15e324
tknk0369 <76672482+tknk0369@users.noreply.github.com>
2022-09-08 23:50:19 +0900

    Create new list dialog

--- lib/screen/todo/todo_drawer.dart
+++ lib/screen/todo/todo_drawer.dart
@@ -17,8 +17,37 @@ class TodoDrawer extends StatelessWidget {
             const Divider(),
             ListTile(
               leading: const Icon(Icons.add),
-              title: const Text("Add new task"),
-              onTap: () {},
+              title: const Text("Add new list"),
+              onTap: () async {
+                await showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: Text("Add new list"),
+                      content: TextField(
+                        autofocus: true,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'List name',
+                        ),
+                      ),
+                      actionsAlignment: MainAxisAlignment.end,
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: Text("Cancel"),
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          child: Text("Ok"),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
             )
           ],
         ),
