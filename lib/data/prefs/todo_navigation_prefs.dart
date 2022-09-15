a4606dc4f01663d1887a07e5e882c14fbfd1dc1e
tknk0369 <76672482+tknk0369@users.noreply.github.com>
2022-09-15 22:58:58 +0900

    Add todo_navigation_prefs

---
+++ lib/data/prefs/todo_navigation_prefs.dart
@@ -0,0 +1,17 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:todolist/constant.dart';
+import 'package:todolist/data/db/app_database.dart';
+
+class TodoNavigationPrefs {
+  static const key = "todo_navigation";
+
+  Future<String> loadTodoNavigation() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(key) ?? inbox.id;
+  }
+
+  Future<void> saveTodoNavigation(TaskList taskList) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(key, taskList.id);
+  }
+}
